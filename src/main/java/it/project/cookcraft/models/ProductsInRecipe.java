77f5f8ac6cf8504a2bbc99df2bc3422d6e8a841b
KikoTheFinker <kristijanstojanovskii24@gmail.com>
2024-07-23 11:56:04 +0200

    update ProductsInRecipe model

--- src/main/java/it/project/cookcraft/models/ProductsInRecipe.java
+++ src/main/java/it/project/cookcraft/models/ProductsInRecipe.java
@@ -7,21 +7,13 @@ import org.springframework.data.relational.core.mapping.Table;
 
 
 @Data
-@Table(name = "products_in_recipe")
 @NoArgsConstructor
 public class ProductsInRecipe {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "product_id")
     private Product product;
 
-    @ManyToOne
-    @JoinColumn(name = "recipe_id")
     private Recipe recipe;
 
-    @Column(name = "measurement")
     private String measurement;
 }
