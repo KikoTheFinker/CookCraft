3b65eaea7997e2c8c326a80043ee09c3e9a2382f
Gorazd Biskoski <gorazdbiskoskii@gmail.com>
2024-09-07 16:10:09 +0200

    Add method for checking user role

--- src/main/java/it/project/cookcraft/dao/interfaces/UserDAO.java
+++ src/main/java/it/project/cookcraft/dao/interfaces/UserDAO.java
@@ -3,6 +3,7 @@ package it.project.cookcraft.dao.interfaces;
 import it.project.cookcraft.dto.UserDTO;
 import it.project.cookcraft.models.Recipe;
 import it.project.cookcraft.models.User;
+import it.project.cookcraft.models.UserType;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,4 +18,5 @@ public interface UserDAO {
     void addRecipeToFavoritesById(Long id, Long recipeId);
     boolean alreadyFavorited(Long id, Long recipeId);
     void removeRecipeFromFavoriteById(Long id, Long recipeId);
+    UserType getUserTypeById(Long userId);
 }
